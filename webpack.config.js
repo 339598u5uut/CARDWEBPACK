fa48b393f06029cb21736f37950a449e26d7835e
Kiva Natalia <violett@mail.ru>
2022-09-16 22:36:06 +0300

    add redmi

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,25 @@
-const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = (env) => ({
-    mode: env.production ? 'production' : 'development',
-    entry: './src/index.js',
+    mode: env.production ? "production" : "development",
+    entry: "./src/index.js",
     output: {
-        filename: 'main.[contenthash].js',
-        publicPath: '/'
+        filename: "main.[contenthash].js",
+        publicPath: "/CARDWEBPACK/",
     },
     performance: {
         hints: false,
         maxEntrypointSize: 512000,
-        maxAssetSize: 512000
+        maxAssetSize: 512000,
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: 'Форма оплаты',
+            title: "Форма оплаты",
         }),
         new MiniCssExtractPlugin({
-            filename: 'main.[contenthash].css',
+            filename: "main.[contenthash].css",
         }),
     ],
     module: {
@@ -27,40 +27,39 @@ module.exports = (env) => ({
                 test: /\.js$/i,
                 exclude: /node_modules/,
                 use: {
-                    loader: 'babel-loader',
+                    loader: "babel-loader",
                     options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
+                        presets: ["@babel/preset-env"],
+                    },
+                },
             },
             {
                 test: /\.png$/i,
-                type: 'asset',
+                type: "asset",
             },
             {
                 test: /\.css$/i,
                 use: [
-                    env.production ? MiniCssExtractPlugin.loader : 'style-loader',
-                    'css-loader',
-                ]
+                    env.production ? MiniCssExtractPlugin.loader : "style-loader",
+                    "css-loader",
+                ],
             },
         ],
     },
     optimization: {
         minimizer: [
-            '...',
+            "...",
             new ImageMinimizerPlugin({
                 minimizer: {
                     implementation: ImageMinimizerPlugin.imageminMinify,
                     options: {
-                        plugins: ['imagemin-optipng'],
-                    }
-                }
-            })
-        ]
+                        plugins: ["imagemin-optipng"],
+                    },
+                },
+            }),
+        ],
     },
     devServer: {
         hot: true,
-    }
-
+    },
 });
